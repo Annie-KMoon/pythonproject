0136056ebba651b6bd8cbfa06445ac8fb171139f
Annie-KMoon <mkw005@naver.com>
2025-09-16 18:11:43 +0900

    0916

---
+++ 04.데이터시각화/web02/app.py
@@ -0,0 +1,105 @@
+#서버 시작 파일은 고정명 app.py
+#전역설정
+import pandas as pd
+df = pd.read_csv('c:/python/04.데이터시각화/data/score.csv')
+
+import matplotlib.pyplot as plt
+#한글폰트 설정
+plt.rc('font', family = 'Noto Sans KR')
+plt.rc('font', size = 15)
+plt.rc('axes', unicode_minus = False)
+
+#라이브러리 import-이미지/앱생성
+from flask import Flask, render_template, send_file
+from io import BytesIO
+
+#앱생성
+app = Flask(__name__, template_folder='temp')
+
+#라우트생성
+@app.route('/')
+def index():
+    return render_template('index.html', title='그래프연습')
+
+#graph1
+@app.route('/graph1')
+def graph1():
+    plt.figure(figsize=(10,5))
+    plt.bar(df['이름'], df['국어'], label='국어',color='#FFFAFA')
+    plt.bar(df['이름'], df['영어'], label='영어', bottom=df['국어'],color='#F8F8FF')
+    plt.bar(df['이름'], df['수학'], label='수학', bottom=df['국어']+df['영어'], color='#F0FFF0')
+    plt.ylim(0,300)
+    plt.legend(loc=(1.02,0.1))
+#이미지저장
+    img =BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0) #이미지 맨앞으로 이동
+    return send_file(img, mimetype='image/png')
+
+#graph2
+@app.route('/graph2')
+def graph2():
+    x=df.index*4
+    plt.figure(figsize=(10,5))
+    plt.bar(x+0, df['국어'], label='국어', width=1,color='#FFFAFA')
+    plt.bar(x+1, df['영어'], label='영어', width=1,color='#F8F8FF')
+    plt.bar(x+2, df['수학'], label='수학', width=1, color='#F0FFF0')
+    plt.legend(loc=(1.02,0.1))
+    xticks = [xtick+1 for xtick in x]
+    plt.xticks(xticks, df['이름'])
+#이미지저장
+    img =BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0) #이미지 맨앞으로 이동
+    return send_file(img, mimetype='image/png')
+
+#graph3
+@app.route('/graph3')
+def graph3():
+    group = df.groupby('SW특기').size()
+    labels = group.index
+    values = group.values
+    colors = ['#FFADAD','#FFD6A5','#FDFFB6','#CAFFBF','#9BF6FF','#A0C4FF']
+    plt.pie(values, labels=labels,
+            explode=[0.02]*len(group),
+            autopct='%.1f%%',colors=colors,
+            wedgeprops= {'width':0.65, 'edgecolor':'ivory', 'alpha':0.3},
+            textprops={'color':'gray'})
+    plt.legend(loc=(1,0.1))
+#이미지저장
+    img =BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0) #이미지 맨앞으로 이동
+    return send_file(img, mimetype='image/png')
+
+#graph4
+@app.route('/graph4')
+def graph4():
+    df['학년'] = [3,3,2,1,1,3,2,2]
+    plt.scatter(df['국어'],df['영어'],sizes=df['학년']*500,
+                c = df['학년'], cmap='autumn_r', alpha=0.3)
+    plt.xlabel('국어')
+    plt.ylabel('영어')
+    plt.colorbar(ticks=[1,2,3], orientation='horizontal', label='학년', shrink=0.5)
+
+    for idx in range(len(df)):
+        x=df.loc[idx,'국어']
+        y=df.loc[idx,'영어']
+        name=df.loc[idx,'이름']
+        plt.text(x,y,name, size=6, ha='center')
+
+    plt.ylim(0,120)
+
+#이미지저장
+    img =BytesIO()
+    plt.savefig(img, format='png')
+    plt.close()
+    img.seek(0) #이미지 맨앞으로 이동
+    return send_file(img, mimetype='image/png')
+
+
+if __name__=="__main__":
+    app.run(port=5000, debug=True)
